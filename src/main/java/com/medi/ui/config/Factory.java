4008712d008e5a3f12393c143cc4f98db0ace538
matsamb <matondo.lusamba@gmail.com>
2022-11-11 20:54:15 +0100

    implemented user interface

---
+++ src/main/java/com/medi/ui/config/Factory.java
@@ -0,0 +1,16 @@
+package com.medi.ui.config;
+
+//import org.apache.catalina.core.ApplicationContext;
+//import org.apache.catalina.core.StandardContext;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Factory {
+
+	//@Bean
+	/*public ApplicationContext getApplicationContext(StandardContext context) {
+		return new ApplicationContext(context);
+	}*/
+	
+}
--- src/main/java/com/medi/ui/proxies/PatientProxy.java
+++ src/main/java/com/medi/ui/proxies/PatientProxy.java
@@ -1,16 +1,24 @@
 package com.medi.ui.proxies;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.medi.ui.bean.PatientBean;
 
-@FeignClient(name="patient", url="localhost:9004")
+//import feign.RequestTemplate;
+/*
+@Repository
+//@FeignClient(name="patient", url="localhost:9004")
 public interface PatientProxy {
 
 	@GetMapping("/patient")
 	ResponseEntity<PatientBean> findPatient(@RequestParam("family") String family, @RequestParam("given") String given);
 	
-}
+	
+}*/
