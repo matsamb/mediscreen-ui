27b34f7274611c66615827d5619c377a47744362
matsamb <matondo.lusamba@gmail.com>
2022-11-18 13:49:03 +0100

    ui implemented

--- src/main/java/com/medi/ui/controller/UserInterfaceController.java
+++ src/main/java/com/medi/ui/controller/UserInterfaceController.java
@@ -2,6 +2,7 @@ package com.medi.ui.controller;
 
 import java.net.http.HttpResponse;
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -17,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
@@ -147,7 +149,7 @@ public class UserInterfaceController {
 	}
 
 	@GetMapping("/findpatient")
-	public String getFindPatient(Model model) {
+	public String getFindPatient(String family, String given, Model model) {
 		return "findpatient";
 	}
 
@@ -201,63 +203,162 @@ public class UserInterfaceController {
 		model.addAttribute("given", given);
 		model.addAttribute("dob", dob);
 		return new ModelAndView("redirect:/viewreport?patientId=" + patientId + "&family=" + family + "&given=" + given
-				+ "&dob=" + dob+"&size="+3+"&page="+1);// +"&report=");
+				+ "&dob=" + dob + "&size=" + 3 + "&page=" + 1);// +"&report=");
 	}
 
 	@GetMapping("/viewreport")
-	public String getViewReport(@RequestParam String patientId, @RequestParam String family, @RequestParam String given,
-			@RequestParam String dob, Model model) {
+	public String getViewReport(@RequestParam String patientId, String family, String given, String dob, Integer size,
+			Integer page, Model model) {
+
+		/*
+		 * public String getViewReport(@RequestParam String patientId, @RequestParam
+		 * String family, @RequestParam String given,
+		 * 
+		 * @RequestParam String dob, @RequestParam Integer size, @RequestParam Integer
+		 * page, Model model) {
+		 */
 
 		log.info("viewreport get " + patientId);
 		Integer integerPatientId = Integer.parseInt(patientId);
 
 		List<ReportBean> reportList = reportProxy.findReportByPatientId(integerPatientId).getBody();
-
-		PagedListHolder<ReportBean> pagedReport = new PagedListHolder<>(reportList);
-
+		/*
+		 * ReportBean testReportBean = new ReportBean(); ReportBean testReportBean2 =
+		 * new ReportBean(); testReportBean.
+		 * setComment("vvvvv hhhh iiiiii oootest comment for today is de rtttunn dsfd poieufnizfn nndksfnskvs qgbubcqfbvsvd"
+		 * ); testReportBean.setId("test"); testReportBean.setDate(new
+		 * Date(System.currentTimeMillis())); testReportBean.setPatientId(1);
+		 * testReportBean2.
+		 * setComment("mon deux test 111112 22222222 3333333333333 6666666 5555555555 8888888888 9999999999"
+		 * ); testReportBean2.setId("test"); testReportBean2.setDate(new
+		 * Date(System.currentTimeMillis())); testReportBean2.setPatientId(1);
+		 * 
+		 * List<ReportBean> reportList = List.of(testReportBean, testReportBean,
+		 * testReportBean, testReportBean2, testReportBean, testReportBean,
+		 * testReportBean2, testReportBean, testReportBean, testReportBean2,
+		 * testReportBean2,
+		 * testReportBean2,testReportBean2,testReportBean,testReportBean,testReportBean
+		 * ,testReportBean2,testReportBean2,testReportBean,testReportBean,testReportBean
+		 * );
+		 */ PagedListHolder<ReportBean> pagedReport = new PagedListHolder<>(reportList);
+		System.out.println(pagedReport);
 		Integer pageSize = 3;
 		// TODO to replace with thymeleaf value
-		Integer activePage = 1;
+		Integer activePage = page;
 
 		pagedReport.setPageSize(pageSize);
-		pagedReport.setPage(activePage);
-		
-		List<Integer> paginationRangeList = List.of(Math.max(activePage - 2, 1), 
-				Math.max(activePage - 1, 1), activePage,
-				Math.min(activePage + 1, pagedReport.getPageCount()),
+		pagedReport.setPage(activePage - 1);
+
+		List<Integer> paginationRangeList = List.of(Math.max(activePage - 2, 1), Math.max(activePage - 1, 1),
+				activePage, Math.min(activePage + 1, pagedReport.getPageCount()),
 				Math.min(activePage + 2, pagedReport.getPageCount()));
 		Set<Integer> paginationRange = paginationRangeList.stream().collect(Collectors.toSet());
-		
-		Set<Integer> pagesSet = IntStream.rangeClosed(1, pagedReport.getPageCount()).boxed().collect(Collectors.toSet());
-			
+		log.info("paginationRange " + paginationRange);
+		Set<Integer> pagesSet = IntStream.rangeClosed(1, pagedReport.getPageCount()).boxed()
+				.collect(Collectors.toSet());
 
-		
-		model.addAttribute("activePage",activePage);
-		model.addAttribute("pagedReport",pagedReport);
-		model.addAttribute("paginationRange",paginationRange);
-		model.addAttribute("numberOfPages",pagedReport.getPageCount());
+		PatientBean patient = patientProxy.findPatient(family, given).getBody();
+
+		model.addAttribute("activePage", activePage);
+		model.addAttribute("pagedReport", pagedReport);
+		model.addAttribute("paginationRange", paginationRange);
+		model.addAttribute("numberOfPages", pagedReport.getPageCount());
 		model.addAttribute("reportList", reportList);
+		model.addAttribute("patient", patient);
+
+		return "viewreport";
+	}
+
+	@GetMapping("/savereport")
+	public String getSaveReport(String patientId, String family, String given, String dob, Model model) {
+
+		PatientBean patientBean = patientProxy.findPatient(family, given).getBody();
+
+		model.addAttribute("patientId", patientBean.getPatientId());
+		model.addAttribute("family", patientBean.getFamily());
+		model.addAttribute("given", patientBean.getGiven());
+		model.addAttribute("dob", patientBean.getDob());
+
+		return "savereport";
+	}
+
+	@PostMapping("/savereport")
+	public ModelAndView saveReport(String report, Integer patientId, String family, String given, String dob,
+			Model model) {
+
+		log.info("savereport");
+		log.info(report);
+		log.info(patientId);
+
+		// Integer integerPatientId = Integer.parseInt(patientId);
+		ReportBean reportBean = new ReportBean();
+		reportBean.setComment(report);
+		reportBean.setPatientId(patientId);
+
+		ReportBean reportBeanCreated = reportProxy.createReport(reportBean).getBody();
+		log.info(reportBeanCreated);
+
 		model.addAttribute("patientId", patientId);
 		model.addAttribute("family", family);
 		model.addAttribute("given", given);
 		model.addAttribute("dob", dob);
 
-		return "viewreport";
+		return new ModelAndView("redirect:/viewreport?patientId=" + patientId + "&family=" + family + "&given=" + given
+				+ "&dob=" + dob + "&size=" + 3 + "&page=" + 1);
 	}
 
-	@GetMapping("savereport")
-	public String getSaveReport() {
-		return "savereport";
-	}
+	@GetMapping("/updatereport")
+	public String getUpdateReport(String id, String report, Integer patientId, String family, String given, String dob,
+			Model model) {
 
-	@GetMapping("updatereport")
-	public String getUpdateReport() {
+		
+		
+		log.info("updatereport");
+		PatientBean patientBean = patientProxy.findPatient(family, given).getBody();		
+		List<ReportBean> reportList = reportProxy.findReportByPatientId(patientId).getBody();
+		ReportBean reportBean = new ReportBean();
+		for (ReportBean r : reportList) {
+			if (r.getId().contentEquals(id)) {
+				reportBean = r;
+			}
+		}
+		log.info(reportBean);
+		log.info("updatereport");
+		
+		model.addAttribute("patientId", patientBean.getPatientId());
+		model.addAttribute("family", patientBean.getFamily());
+		model.addAttribute("given", patientBean.getGiven());
+		model.addAttribute("dob", patientBean.getDob());
+		model.addAttribute("id", reportBean.getId());
+		model.addAttribute("report", reportBean.getComment());
 		return "updatereport";
 	}
 
-	@GetMapping("deletereport")
-	public String getDeleteReport() {
-		return "deletereport";
+	@PostMapping("/updatereport")
+	public ModelAndView updateReport(String id ,String report, Integer patientId, String family, String given, String dob,
+			Model model) {
+
+		PatientBean patientBean = patientProxy.findPatient(family, given).getBody();	
+		
+		log.info("updatereport");
+		log.info("updatereport comment "+report);
+		List<ReportBean> repBeanList = reportProxy.findReportByPatientId(patientId).getBody().stream().filter(r -> r.getId().equals(id)).toList();	
+		log.info("updatereport returnedReport "+repBeanList);
+		ReportBean repBean = repBeanList.get(0);
+		repBean.setComment(report);
+		ReportBean reportBeanCreated = reportProxy.updateReport(patientId, repBean).getBody();
+		log.info("updatereport reportBeanCreated "+reportBeanCreated);
+		
+		model.addAttribute("patient",patientBean);
+		model.addAttribute("patientId", patientId);
+		model.addAttribute("family", family);
+		model.addAttribute("given", given);
+		model.addAttribute("dob", dob);
+		model.addAttribute("id", repBean.getId());
+		model.addAttribute("report", repBean.getComment());
+
+		return new ModelAndView("redirect:/viewreport?patientId=" + patientId + "&family=" + family + "&given=" + given
+				+ "&dob=" + dob + "&size=" + 3 + "&page=" + 1);
 	}
 
 }
